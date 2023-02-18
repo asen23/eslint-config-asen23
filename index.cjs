7ff00c6a04eda5cb0e4192d77285bc6110b734f0
asen23 <andersenlim23@gmail.com>
2023-02-18 23:20:24 +0700

    Create eslint config

---
+++ index.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        "singleQuote": true,
+        "tabWidth": 2,
+        "trailingComma": "es5",
+        "useTabs": false,
+        "bracketSpacing": true,
+        "semi": true,
+      },
+    ],
+  },
+}
